0bc1666a8a0e305289fec90439704cd42e9b418d
Aman Maharjan <amanmaharjan@Amans-MacBook-Air.local>
2021-03-17 03:03:34 +0545

    event models

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+FOCUS = (
+    ('T', 'Tech'),
+    ('M', 'Marketing'),
+    ('C', 'Content Creation')
+)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
@@ -10,4 +15,21 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}\'s Profile '
-    
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.CharField(max_length=25)
+    speakers = models.CharField(max_length=250)
+    focus = models.CharField(
+        max_length=1,
+        choices=FOCUS,
+        default=FOCUS[0][0]
+    )
+    description = models.TextField()
+    url = models.CharField(
+        max_length=250,
+        default='https://us05web.zoom.us/j/7915630422?pwd=Q2hnV2NnUEdFVTNYNUJRTWtHdDlzZz09'
+        )
+    def __str__(self):
+        return self.title
