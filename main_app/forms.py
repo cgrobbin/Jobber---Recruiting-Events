ea8fcc4e0b8d5d35a105bebd58541769ff4dc7ef
Aman Maharjan <amanmaharjan@Amans-MacBook-Air.local>
2021-03-17 00:56:20 +0545

    works on creating the profile model, user can now change their username and email attribute when they are log in

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,3 +11,15 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['email', 'username', 'password1', 'password2']
+
+# this form is used to update the content of the field of username and email of the login user
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields=['bio']
+
