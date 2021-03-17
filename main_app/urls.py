f1a422dc1ac268543e6e2cf7164b929e3d486797
cgrobbin <cgrobbin@gmail.com>
2021-03-17 11:18:18 -0500

    Added search functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('register/',views.register,name='register'),
     path('accounts/profile/',views.profile, name = 'profile'),
     path('about/', views.about, name='about'),
+    path('search/',views.search, name = 'search'),
+    path('searchoption/',views.searchoption,name = 'seachoption'),
     path('events/<int:event_id>/', views.event_detail, name='detail'),
     path('events/<int:event_id>/add_registration/', views.add_registration, name='add_registration'),
 ]
